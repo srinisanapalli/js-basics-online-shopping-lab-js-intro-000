28ae7d0636c43cb8a54d75722be8a47a8bdcc05c
Srinivas Sanapalli <srinisanapalli@gmail.com>
2016-08-14 20:05:15 +0000

    Done.

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ function removeFromCart(item){
 };
 
 function placeOrder(cardNumber){
-  if (cardNumber != undefined){
+  if (typeof(cardNumber)!= 'undefined'){
     console.log(`Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`);
   }
   else{
