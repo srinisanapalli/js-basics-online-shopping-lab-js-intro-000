0a98e92171bc633a57fe364601e6c7251e62a26f
Srinivas Sanapalli <srinisanapalli@gmail.com>
2016-08-15 21:19:30 +0000

    Done.

--- index.js
+++ index.js
@@ -64,7 +64,6 @@ function removeFromCart(item){
 };
 
 function placeOrder(cardNumber){
-
   if (cardNumber){
     console.log(`Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`);
   }
